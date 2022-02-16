62a0fbdbddb259dac76d7e6b30673cc75f566bc1
Berkay <mberkaybulut@gmail.com>
2022-02-16 01:34:13 -0500

    Update UniswapV2Library.sol

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.5;
 
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 
